e7473707d1ddf6d0112e8d92d2806ffd7bc6d263
Sean Huang <huangtx2002@hotmail.com>
2024-08-21 21:29:04 +0000

    Added back crowdfunding

---
+++ crowdfunding/frontend/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import { useEffect } from 'react';
+
+const App = () => {
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const {solana} = window;
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!");
+          const response = await solana.connect({onlyIfTrusted: true});
+          console.log("Connected with public key:", response.publicKey.toString());
+          if (response) {
+            console.log('Connected to wallet');
+          } else {
+            console.log('Not connected to wallet');
+          }
+        }
+      } else {
+        console.log("Solana object not found! Get a Phantom wallet!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const connectWallet = async () => {};
+  const renderNotConnectedContainer = () => {
+    <button onClick={connectWallet}>Connect Wallet</button>;
+  };
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  return <div className="App">{renderNotConnectedContainer()}</div>;
+};
+
+export default App;
