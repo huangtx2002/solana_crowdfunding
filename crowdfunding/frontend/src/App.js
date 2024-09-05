379f9299e4c33f03e2b6ade5daa324ea91ff9832
Sean Huang <huangtx2002@hotmail.com>
2024-09-05 20:30:08 +0000

    Added frontend

--- crowdfunding/frontend/src/App.js
+++ crowdfunding/frontend/src/App.js
@@ -1,17 +1,35 @@
 import './App.css';
-import { useEffect } from 'react';
+import idl from './idl.json';
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Program, AnchorProvider, web3, utils, BN, } from '@project-serum/anchor';
+import { useEffect, useState } from 'react';
+
+const programID = new PublicKey(idl.address);
+const network = clusterApiUrl('devnet');
+const opts = {
+  preflightCommitment: "processed",
+  commitment: "processed",
+};
+const { SystemProgram } = web3;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new AnchorProvider(connection, window.solana, opts.preflightCommitment);
+    return provider;
+  }
   const checkIfWalletIsConnected = async () => {
     try {
       const {solana} = window;
       if (solana) {
         if (solana.isPhantom) {
           console.log("Phantom wallet found!");
-          const response = await solana.connect({});
-          console.log("Connected with public key:", response.publicKey.toString());
+          const response = await solana.connect({onlyIfTrusted: true});
           if (response) {
+            console.log("Connected with public key:", response.publicKey.toString());
             console.log('Connected to wallet');
+            setWalletAddress(response.publicKey.toString());
           } else {
             console.log('Not connected to wallet');
           }
@@ -23,9 +41,61 @@ const App = () => {
       console.error(error);
     }
   };
-  const connectWallet = async () => {};
+  const connectWallet = async () => {
+    const {solana} = window;
+    if (solana) {
+      if (solana.isPhantom) {
+        const response = await solana.connect();
+        if (response) {
+          console.log("Connected with public key:", response.publicKey.toString());
+          console.log('Connected to wallet');
+          setWalletAddress(response.publicKey.toString());
+        } else {
+          console.log('Not connected to wallet');
+        }
+      } else {
+        console.log("Solana object not found! Get a Phantom wallet!");
+      }
+    }
+  };
+
+  const createCampaign = async () => {
+    try {
+      const provider = getProvider();
+      console.log("Provider:", provider);
+      console.log("Program ID:", programID.toString());
+      console.log("IDL:", idl);
+
+      const program = new Program(idl, programID, provider);
+      console.log("Program instantiated:", program);
+
+      const [campain] = await web3.PublicKey.findProgramAddress(
+          [
+            utils.bytes.utf8.encode("CAMPAIGN_DEMO"),
+            provider.wallet.publicKey.toBuffer(),
+          ],
+          program.programId
+        );
+        console.log("Campaign address:", campain.toString());
+        
+        await program.rpc.create("campaign name", "campaign description", {
+          accounts: {
+            campain,
+            user: provider.wallet.publicKey,
+            systemProgram: SystemProgram.programId,
+          }
+        });
+        console.log("Created a new campain w/ address:", campain.toString());
+    } catch (error) {
+      console.error("Errpr creating campain account:", error);
+    }
+  };
+
   const renderNotConnectedContainer = () => {
-    <button onClick={connectWallet}>Connect Wallet</button>;
+    return <button onClick={connectWallet}>Connect Wallet</button>;
+  };
+  const renderConnectedContainer = () => {
+    return <button onClick={createCampaign}>Create a campaign</button>;
   };
   useEffect(() => {
     const onLoad = async () => {
@@ -35,7 +105,9 @@ const App = () => {
     return () => window.removeEventListener("load", onLoad);
   }, []);
 
-  return <div className="App">{renderNotConnectedContainer()}</div>;
+  return <div className="App">
+      {walletAddress ? renderConnectedContainer() : renderNotConnectedContainer()}
+    </div>;
 };
 
 export default App;
